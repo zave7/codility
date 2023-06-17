1204c6fb035955dd7b1014fe1197320dee16b3c2
zave7 <zave7@naver.com>
2023-06-17 17:48:00 +0900

    Lesson4 MaxCounters

---
+++ src/training/MaxCounters.kt
@@ -0,0 +1,23 @@
+package training
+
+class MaxCounters {
+    fun solution(N: Int, A: IntArray): IntArray {
+        val result = IntArray(N)
+        var max = 0
+        var currMax = 0
+        for(value in A) {
+            val index = value - 1
+            if(value == N + 1) {
+                max = currMax
+            } else {
+                result[index] = result[index].coerceAtLeast(max) + 1
+                currMax = result[index].coerceAtLeast(currMax)
+            }
+        }
+        for(i in result.indices) {
+            if(result[i] < max)
+                result[i] = max
+        }
+        return result
+    }
+}
