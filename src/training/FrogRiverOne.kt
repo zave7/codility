bdf080946b8bc20f02b9bed77c3ca94670bc82cb
zave7 <zave7@naver.com>
2023-06-17 16:32:54 +0900

    Lesson4 FrogRiverOne

---
+++ src/training/FrogRiverOne.kt
@@ -0,0 +1,13 @@
+package training
+
+class FrogRiverOne {
+    fun solution(X: Int, A: IntArray): Int {
+        val set = HashSet<Int>()
+        for(n in A.indices) {
+            set.add(A[n])
+            if(set.size == X)
+                return n
+        }
+        return -1
+    }
+}
