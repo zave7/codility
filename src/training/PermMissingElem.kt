925ecf02b8cddab781ab451fab4956a00211cd7c
zave7 <zave7@naver.com>
2023-06-17 15:27:57 +0900

    Lesson3 PermMissingElem

---
+++ src/training/PermMissingElem.kt
@@ -0,0 +1,8 @@
+package training
+
+class PermMissingElem {
+    fun solution(A: IntArray): Int {
+        val total = ((A.size + 1).toLong() * (A.size + 2)) / 2
+        return (total - A.sum()).toInt()
+    }
+}
