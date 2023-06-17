1b2b7eaf2219ff279b0ca1992b573c418b4f1b07
zave7 <zave7@naver.com>
2023-06-17 18:24:00 +0900

    Lesson4 MissingInteger

---
+++ src/training/MissingInteger.kt
@@ -0,0 +1,13 @@
+package training
+
+class MissingInteger {
+    fun solution(A: IntArray): Int {
+        var min = 1
+        A.sort()
+        for(n in A) {
+            if(min == n)
+                min++
+        }
+        return min
+    }
+}
