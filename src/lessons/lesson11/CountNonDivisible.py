918b7b3c79b319bb9613928879fb41d7b642dddc
zave7 <if.zave@gmail.com>
2023-10-03 19:59:37 +0900

    Lesson11 CountNonDivisible

--- src/lessons/lesson11/CountNonDivisible.py
+++ src/lessons/lesson11/CountNonDivisible.py
@@ -3,12 +3,12 @@ def solution(A):
     element = [0 for _ in range(2 * N + 1)]
     for a in A:
         element[a] += 1
-    answer = [0 for _ in range(N)]
+    result = [0 for _ in range(N)]
     acc = [-1 for _ in range((2 * N + 1))]
     for i in range(N):
         val = A[i]
         if acc[val] != -1:
-            answer[i] = acc[val]
+            result[i] = acc[val]
         else:
             count = 0
             for target in range(1, int(val ** 0.5) + 1):
@@ -16,6 +16,6 @@ def solution(A):
                     count += element[target]
                     if target != val // target:
                         count += element[val // target]
-            answer[i] = N - count
-            acc[val] = answer[i]
-    return answer
+            result[i] = N - count
+            acc[val] = result[i]
+    return result
